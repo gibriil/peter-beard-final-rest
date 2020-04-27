37b302563f5e1fb5aec0169c11f9a00f439fe705
Gibriil <musicmandanceman@yahoo.com>
2020-04-27 15:45:50 -0600

    removed some custom cors stuff

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: '*'
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//     );
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.use('/', router);
 
