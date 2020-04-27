4ef359b0c5c82a16a18621e31820097014dd437b
Gibriil <musicmandanceman@yahoo.com>
2020-04-27 15:51:34 -0600

    Corrected CORS

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: '*'
+    origin: false
 }));
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
